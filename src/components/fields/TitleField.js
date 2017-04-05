1bcc297d177ecf5792114ee3e62647f5429aba97
Santiago Quintana <squintana@carecloud.com>
2017-04-05 09:59:09 -0400

    ADD anchor tag to TitleField legend

--- src/components/fields/TitleField.js
+++ src/components/fields/TitleField.js
@@ -5,7 +5,8 @@ const REQUIRED_FIELD_SYMBOL = "*";
 function TitleField(props) {
   const {id, title, required} = props;
   const legend = required ? title + REQUIRED_FIELD_SYMBOL : title;
-  return <legend id={id}>{legend}</legend>;
+  const anchor = "#" + legend
+  return <legend id={id}><a href={anchor}>{legend}</a></legend>;
 }
 
 if (process.env.NODE_ENV !== "production") {
