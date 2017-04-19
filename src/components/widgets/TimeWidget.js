aeaa3c74f88c4fc11d4376541aefcb37b7f82808
Santiago Quintana <squintana@carecloud.com>
2017-04-19 13:11:02 -0400

    ADD pad time if needed

--- src/components/widgets/TimeWidget.js
+++ src/components/widgets/TimeWidget.js
@@ -2,6 +2,14 @@ import React, {PropTypes} from "react";
 
 import BaseInput from "./BaseInput";
 
+function padTimeIfNeeded(time) {
+  let splitTime = time.split(":")
+  if (splitTime.length === 2) {
+    splitTime.push("00")
+  }
+  return splitTime.join(":")
+}
+
 
 function TimeWidget(props) {
   const {onChange} = props;
@@ -9,7 +17,7 @@ function TimeWidget(props) {
     <BaseInput
       type="time"
       {...props}
-      onChange={(value) => onChange(value || undefined)}/>
+      onChange={(value) => onChange(padTimeIfNeeded(value) || undefined)}/>
   );
 }
 
